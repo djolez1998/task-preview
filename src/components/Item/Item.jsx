1d36e62424b9cdc9df8a802f74d173857a6808f1
Djole <djole98sp@live.com>
2020-08-20 20:43:35 +0200

    added style

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Item.scss";
 
-const Item = ({ task }) => <div>{task.title}</div>;
+const Item = ({ task }) => <div className='item'>{task.title}</div>;
 
 export default Item;
